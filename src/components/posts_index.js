bd5cbd7ff438bf2cd656a0aeb8c71d0cfd6de40d
Do-Dom <yunjeong.syj@gmail.com>
2018-04-21 19:34:14 +0900

    connect action creator and component(edit posts_index,js)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +16,5 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+//mapdetach어쩌구 함수안쓰고 바로 action에서 fetch할거(action creator) 이름 써도 됨. 이 과정에서 넘길 이름과 내용이 같아서 es6로 축약함
+export default connect(null, { fetchPosts }) PostsIndex;
