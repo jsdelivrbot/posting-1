be169df3db86f4c50e3dfe7b1f5c2476eb88ba96
Do-Dom <yunjeong.syj@gmail.com>
2018-04-21 19:09:08 +0900

    edit actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
 //network에서 정보받아오려면 axios랑 redux-promise사용하기 때문에 둘 다 설치 해야해.
+import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=dodom7';
+
 export funtion fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
   return {
-      type: FETCH_POSTS
+      type: FETCH_POSTS,
+      payload: request
   };
 }
