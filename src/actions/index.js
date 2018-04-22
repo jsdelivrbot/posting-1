a8356bba7075b1d8d3fb48288eae756e2e67e48b
Do-Dom <yunjeong.syj@gmail.com>
2018-04-22 22:52:13 +0900

    fix error in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_POSTS = 'fetch_posts';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=dodom7';
 
-export funtion fetchPosts() {
+export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -17,4 +17,4 @@ class PostsIndex extends Component {
 }
 
 //mapdetach어쩌구 함수안쓰고 바로 action에서 fetch할거(action creator) 이름 써도 됨. 이 과정에서 넘길 이름과 내용이 같아서 es6로 축약함
-export default connect(null, { fetchPosts }) PostsIndex;
+export default connect(null, { fetchPosts }) (PostsIndex);
