4ccabea7727828e8b9aaa38797ac39e32437c839
Do-Dom <yunjeong.syj@gmail.com>
2018-04-23 22:14:51 +0900

    selecting from ownprops

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+ //automatically render
+ //fetching individual post with given id
+  componentDidMount() {
+    const { id } = this.props.match.params.id; //id라는 wild card에 접근?관련된 함수래
+    this.props.fetchPost(id);
+  }
+
   render() {
+    this.props === ownProps;
     return (
       <div>
         Posts Show!
@@ -10,4 +20,8 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) { //모르겟는데 146.
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost }) (PostsShow);
