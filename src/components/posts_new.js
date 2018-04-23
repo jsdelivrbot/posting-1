7025046a9b95a11d10b4422a764d210a0c7935c5
Do-Dom <yunjeong.syj@gmail.com>
2018-04-23 16:58:50 +0900

    add validate() to show errors to user

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -7,22 +7,24 @@ class PostsNew extends Component {
   renderField(field) {
     //비슷한 함수가 반복되니까 중복되게 안쓰려고 고칠거임
 
-    //return jsx
-
     //field라는 object에는 event handler가 포함되어 있어서
     //이 event handler는 return할 jsx에 wired up될것임.
     //그래서 Field랑 연결되는거
 
-    return (
+    return (     //return jsx
       <div className="form-group">
         <label>{field.label}</label>
         <input
           className="form-control"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     );
   }
+  //{field.meta.error}
+  //validate function에 있는 errors의 property의 name과 같은 field의 property가 있으면 error띄워줌
+  //foeld.name이 바뀌면 validate 함수의 error.name도 바뀌어야 함
 
 
   render() {
@@ -48,7 +50,26 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  //validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+  //If errors is empty, the form is fine to submit
+  //If errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 }) (PostsNew);
 
