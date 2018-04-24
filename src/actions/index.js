10caa186317fa029c96eb85e85d42acc4ef9b6c0
Do-Dom <yunjeong.syj@gmail.com>
2018-04-24 16:43:57 +0900

    add delete func

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -11,7 +12,6 @@ const API_KEY = '?key=dodom7';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
   return {
       type: FETCH_POSTS,
       payload: request
@@ -31,9 +31,17 @@ export function createPost(values, callback) {
 
 export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
-
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
