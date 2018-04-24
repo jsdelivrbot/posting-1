d0b9717adc3ace10f1d8b960eb5322949d797bb1
Do-Dom <yunjeong.syj@gmail.com>
2018-04-24 16:09:43 +0900

    add postview link at post title and caching records

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -6,9 +6,12 @@ import { fetchPost } from '../actions';
 class PostsShow extends Component {
  //automatically render
  //fetching individual post with given id
-  componentDidMount() {
-    const { id } = this.props.match.params; //id라는 wild card에 접근?관련된 함수래
-    this.props.fetchPost(id);
+
+   componentDidMount() {
+    if(!this.props.post) { //포스트보러 들어갈때 중복으로 받아오는 일 없게. optional
+      const { id } = this.props.match.params; //id라는 wild card에 접근?관련된 함수래
+      this.props.fetchPost(id);
+    }
   }
 
   render() {
